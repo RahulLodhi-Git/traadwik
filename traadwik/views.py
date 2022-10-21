a668dbfbbc5950a2f07344473390bca631669d00
Rahul Lodhi <rahul.lodhi5003@gmail.com>
2022-10-21 10:46:42 +0530

    Contact page UI done

--- traadwik/views.py
+++ traadwik/views.py
@@ -1,5 +1,17 @@
+import re
 from django.shortcuts import render;
 
 
 def index(request):
     return render(request,'index.html')
+
+# contact
+def contact(request):
+    return render(request,'contact.html')
+
+# About
+def about(request):
+    return render(request,'about.html')
+
+def careers(request):
+    return render(request, 'careers.html')
